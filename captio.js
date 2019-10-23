8502fbc31266c804929844a4a4e1fdca13eaabb4
Iván Coleto <ivancoleto@gmail.com>
2019-10-23 16:22:32 +0200

    add captio script for filling tickets on platform

---
+++ captio.js
@@ -0,0 +1,44 @@
+let PROJECT_CODE = "PYXXXXX";
+let CLIENT = "Google";
+let MOTIVO_COMIDA = "Lunch";
+let NUMERO_COMENSALES = 1;
+
+$('[id*="revisar')[0].click();
+var interval = setInterval(() => {
+  if (
+    $("#formaPagoTicket option").length > 2 &&
+    $("#conceptoTicket option").length > 2 &&
+    $("#cf_5_9670 option").length > 2
+  ) {
+    console.log("now it's loaded!");
+
+    // Concepto ticket - comida
+    $("#conceptoTicket").val(283895);
+    $("#conceptoTicket").change();
+
+    //facturable s/n
+    $("#cf_5_9670").val(242420);
+    $("#cf_5_9670").change();
+
+      
+    // codigo proyecto
+    $("#cf_1_9606").val(PROJECT_CODE);
+
+    //cliente
+    $("#cf_1_9848").val(CLIENT);
+
+    //motivo de la comida
+    $("#cf_1_9744").val(MOTIVO_COMIDA);
+
+    //numero de comensales
+    $("#cf_3_9745").val(NUMERO_COMENSALES);
+
+    //facturable s/n
+    $("#cf_5_9670").val(242420);
+    $("#cf_5_9670").change();
+
+    $(".process").click();
+
+    clearInterval(interval);
+  }
+}, 100);
